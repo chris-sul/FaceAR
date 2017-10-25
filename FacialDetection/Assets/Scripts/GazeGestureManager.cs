3ba37d1ece79cfd8138092eb9b31f45487f904f1
codemastermd <codemastermd@gmail.com>
2017-10-25 16:00:36 -0400

    trying to get working click functionality

---
+++ FacialDetection/Assets/Scripts/GazeGestureManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GazeGestureManager : MonoBehaviour {
+
+    public static GazeGestureManager Instance { get; private set; }
+
+    // Represents the hologram that is currently being gazed at.
+    public GameObject FocusedObject { get; private set; }
+	Text identifyText;
+
+    UnityEngine.XR.WSA.Input.GestureRecognizer recognizer;
+
+    // Use this for initialization
+    void Start()
+    {
+        Instance = this;
+		identifyText = GameObject.Find ("Text").GetComponent<Text>();
+
+        // Set up a GestureRecognizer to detect Select gestures.
+        recognizer = new UnityEngine.XR.WSA.Input.GestureRecognizer();
+        recognizer.TappedEvent += (source, tapCount, ray) =>
+        {
+            // Send an OnSelect message to the focused object and its ancestors.
+           // if (FocusedObject != null)
+           // {
+           //     FocusedObject.SendMessageUpwards("OnSelect");
+           // }
+
+			identifyText.text = "Taking picture";
+        };
+        recognizer.StartCapturingGestures();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Figure out which hologram is focused this frame.
+        GameObject oldFocusObject = FocusedObject;
+
+        // Do a raycast into the world based on the user's
+        // head position and orientation.
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            // If the raycast hit a hologram, use that as the focused object.
+            FocusedObject = hitInfo.collider.gameObject;
+        }
+        else
+        {
+            // If the raycast did not hit a hologram, clear the focused object.
+            FocusedObject = null;
+        }
+
+        // If the focused object changed this frame,
+        // start detecting fresh gestures again.
+        if (FocusedObject != oldFocusObject)
+        {
+            recognizer.CancelGestures();
+            recognizer.StartCapturingGestures();
+        }
+    }
+}
---
+++ FacialDetection/Assets/Scripts/InputCommands.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputCommands : MonoBehaviour {
+
+	void onSelect() {
+		if (!this.GetComponent<Rigidbody> ()) {
+			var rigidbody = this.gameObject.AddComponent<Rigidbody> ();
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
